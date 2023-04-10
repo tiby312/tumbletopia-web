de62a54006b205e2ff6a6a51879b445c07e4c145
Kenneth Reed <kenakioreed@gmail.com>
2023-04-09 20:32:24 -0400

    newv

--- pkg/gridlock.js
+++ pkg/gridlock.js
@@ -896,7 +896,7 @@ function getImports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper445 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper443 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 110, __wbg_adapter_22);
         return addHeapObject(ret);
     };
